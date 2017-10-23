50d267815d6a2fba9897cfefee225958515d930f
Jason Domizio <jdomizio@advent.com>
2017-10-23 18:03:01 -0400

    get example

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,6 +2,13 @@ const restify = require('restify');
 
 const server = restify.createServer();
 
+server.get('/hello', (req, res, next) => {
+    res.send({
+        hello: 'world',
+    });
+    next();
+});
+
 server.listen('8080', () => {
     console.log(`${server.name} listening at ${server.url}`);
 });
